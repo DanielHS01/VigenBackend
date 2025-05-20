e5829371a366fa3eb5772f15a90135855cbe14dd
DanielHS <danielhs2908@gmail.com>
2025-05-19 21:10:48 -0500

    Poll Functionality

--- VigenBackend/Controllers/PollController.cs
+++ VigenBackend/Controllers/PollController.cs
@@ -32,27 +32,6 @@ namespace Vigen_Repository.Controllers
             try
             {
                 await _context.Polls.AddAsync(poll);
-                
-                ViolenceType violenceType = await _context.ViolenceTypes.FindAsync(poll.P7);
-                User user = await _context.Users.FindAsync(poll.UserId);
-                Notify notify = new Notify
-                {
-                    UserId = poll.UserId,
-                    Title = "VIGEN - Posible caso de: " + violenceType.Name,
-                    Description = "Se ha reportado un POSIBLE caso de " + violenceType.Name +
-                    " Identificacion: " + poll.UserId +
-                    " Nombre: " + user.Name +
-                    " Ubicación: " + user.Ubication,
-                    OrganizationTypeId = 1,
-                    StateId = 0
-                };
-                
-                
-                if (poll.P7 != 0)
-                {
-                    await _hubContext.Clients.All.recibeNotify(notify);
-                    await _context.Notifies.AddAsync(notify);
-                }
                 await _context.SaveChangesAsync();
 
                 return Ok(poll);
